80fdd7f3d2a0f505ffd3fc3f6566239c1e6fb928
Brenda Pereira <brendacamara@outlook.com>
2022-08-28 23:28:04 -0300

    feat: add vagao class

---
+++ Vagao.java
@@ -0,0 +1,36 @@
+
+public class Vagao {
+    private double capacidadeCarga;
+    private int identificador;
+
+    public Vagao(int identificador, double capacidadeCarga) {
+        this.identificador = identificador;
+        this.capacidadeCarga = capacidadeCarga;
+    }
+    
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public double getCapacidadeCarga() {
+        return capacidadeCarga;
+    }
+
+    /* public Trem getTrem(){
+        return new Trem()
+    } */
+
+    public boolean livre() {
+        return true; // WTF O QUE TA LIVRE?
+    }
+
+    public void setTrem(Trem trem){
+        return
+    }
+
+    @Override
+    public String toString() {
+        // O que fazer aqui? me dê regra de negócio.
+        return super.toString();
+    }
+}
