f9c7fe7b989dea9cfe68cb96e552e7d23fcfadc0
Lucca <luccatisserparadeda@gmail.com>
2022-08-28 23:58:16 -0300

    feat: sleep

--- Vagao.java
+++ Vagao.java
@@ -1,36 +1,38 @@
 
 public class Vagao {
-    private double capacidadeCarga;
-    private int identificador;
+	private double capacidadeCarga;
+	private int identificador;
 
-    public Vagao(int identificador, double capacidadeCarga) {
-        this.identificador = identificador;
-        this.capacidadeCarga = capacidadeCarga;
-    }
-    
-    public int getIdentificador() {
-        return identificador;
+	public Vagao(int identificador, double capacidadeCarga) {
+		this.identificador = identificador;
+		this.capacidadeCarga = capacidadeCarga;
+	}
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public double getCapacidadeCarga() {
+		return capacidadeCarga;
+	}
+
+	/*
+	 * public Trem getTrem(){
+	 * return new Trem()
+	 * }
+	 */
+
+	public boolean livre() {
+		return true; // WTF O QUE TA LIVRE?
+	}
+
+	public void setTrem(Trem trem){
+        return 
     }
 
-    public double getCapacidadeCarga() {
-        return capacidadeCarga;
-    }
-
-    /* public Trem getTrem(){
-        return new Trem()
-    } */
-
-    public boolean livre() {
-        return true; // WTF O QUE TA LIVRE?
-    }
-
-    public void setTrem(Trem trem){
-        return
-    }
-
-    @Override
-    public String toString() {
-        // O que fazer aqui? me dê regra de negócio.
-        return super.toString();
-    }
+	@Override
+	public String toString() {
+		// O que fazer aqui? me dê regra de negócio.
+		return super.toString();
+	}
 }
